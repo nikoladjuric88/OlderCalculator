a7e582f131a6e4a08e444f1c5cab7921ba366bee
nikoladjuric88 <djura.@gmail.com>
2017-03-05 22:46:50 +0100

    newchange

--- racunaljka.js
+++ racunaljka.js
@@ -1,38 +1,106 @@
-/*=================================== brojevi==================================================*/
-
 var writes = document.getElementById('screen');
+var account = '';
 
-var addNumbers = document.querySelector('.allNumbers');
-var addEveryNumber = addNumbers.querySelectorAll('.number');
+var buttons = document.querySelector('.allButtons');
+var numberButtons = buttons.querySelectorAll('.number');
 
+for (var i = 0; i <= numberButtons.length - 1; i++) {
 
-for (var i = 0; i <= addEveryNumber.length - 1; i++) {
+    numberButtons[i].onclick = function() {
 
-    addEveryNumber[i].onclick = function() {
+        account += this.innerHTML;
 
-        writes.value += this.innerHTML;
-
-        if (writes.value[0] === '0') {
-            writes.value = "";
+        if (account[0] === '0') {
+            account = "";
         }
+        writes.value = account;
         writes.focus();
+        console.log(account);
+        /*======================================================================================================================*/
+        var first = account.indexOf("-");
+        var second = account.indexOf("-", 1);
+
+        if (account[first] === '-' && account[second] === '-') {
+            num1 = account.substring(0, second);
+            num2 = account.substring(second + 1);
+
+            console.log(num1);
+
+            writes.value = num2;
+        }
+
+        var textLength = account.length;
+
+        for (var i = 0; i < textLength; i++) {
+
+            if (account[i] === '+') {
+                num1 = account.substring(0, account.indexOf('+'));
+                num2 = account.substring(account.indexOf('+') + 1);
+
+                console.log(num1);
+
+                writes.value = num2;
+            }
+
+            if (account[i] === 'x') {
+                num1 = account.substring(0, account.indexOf('x'));
+                num2 = account.substring(account.indexOf('x') + 1);
+
+                console.log(num1);
+
+                writes.value = num2;
+            }
+
+            if (account[i] === '/') {
+                num1 = account.substring(0, account.indexOf('/'));
+                num2 = account.substring(account.indexOf('/') + 1);
+
+                console.log(num1);
+
+                writes.value = num2;
+            }
+        }
     }
 };
+/*======================================================================================================================*/
+var operationButtons = buttons.querySelectorAll('.operation');
 
-/*=================================== brojevi==================================================*/
-/*=================================== operacije================================================*/
+for (var i = 0; i <= operationButtons.length - 1; i++) {
 
-var addOperations = addNumbers.querySelectorAll('.operation');
+    operationButtons[i].onclick = function() {
 
-for (var i = 0; i <= addOperations.length - 1; i++) {
+        if (!isNaN(account)) {
+            account += this.innerHTML;
+            console.log(account);
 
-    addOperations[i].onclick = function() {
-
-        if (!isNaN(writes.value)) {
-            writes.value += this.innerHTML;
         }
-        if (writes.value[0] === '-' || writes.value[0] === '+' || writes.value[0] === 'x' || writes.value[0] === '/') {
-            writes.value = "";
+        if (account[0] === '+' || account[0] === 'x' || account[0] === '/') {
+            account = "";
+
+        }
+
+        if (account[first] === '-' && account[second] === '-') {
+
+            writes.value = num1;
+        }
+
+        var textLength = account.length;
+        for (var j = 0; j < textLength; j++) {
+
+            if (account[j] === '+') {
+
+                writes.value = num1;
+            }
+
+            if (account[i] === 'x') {
+
+                writes.value = num2;
+            }
+
+            if (account[i] === '/') {
+
+                writes.value = num2;
+            }
         }
         writes.focus();
     }
@@ -42,59 +110,63 @@ var btnEquality = document.getElementById("equality");
 
 btnEquality.onclick = function() {
 
-    var textLength = writes.value.length;
+    var textLength = account.length;
+    var first = account.indexOf("-");
+    var second = account.indexOf("-", 1);
+
+    if (account[first] === '-' && account[second] === '-') {
+        num1 = account.substring(0, second);
+        num2 = account.substring(second + 1);
+        account = num1 - num2;
+        writes.value = account;
+    }
     for (var i = 0; i < textLength; i++) {
-        if (writes.value[i] === '-') {
-            writes.value1 = writes.value.substring(0, writes.value.indexOf('-'));
-            writes.value2 = writes.value.substring(writes.value.indexOf('-') + 1);
-            writes.value = writes.value1 - writes.value2;
-        } else if (writes.value[i] === '+') {
-            writes.value1 = writes.value.substring(0, writes.value.indexOf('+'));
-            writes.value2 = writes.value.substring(writes.value.indexOf('+') + 1);
-            writes.value = Number(writes.value1) + Number(writes.value2);
-        } else if (writes.value[i] === 'x') {
-            writes.value1 = writes.value.substring(0, writes.value.indexOf('x'));
-            writes.value2 = writes.value.substring(writes.value.indexOf('x') + 1);
-            writes.value = writes.value1 * writes.value2;
-        } else if (writes.value[i] === '/') {
-            writes.value1 = writes.value.substring(0, writes.value.indexOf('/'));
-            writes.value2 = writes.value.substring(writes.value.indexOf('/') + 1);
-            writes.value = writes.value1 / writes.value2;
+
+        if (account[i] === '+') {
+
+            account = Number(num1) + Number(num2);
+            writes.value = account;
+        }
+
+        if (account[i] === 'x') {
+            account = num1 * num2;
+            writes.value = account;
+        }
+        if (account[i] === '/') {
+            account = num1 / num2;
+            writes.value = account;
         }
     }
     writes.focus();
 };
-
-/*=================================== operacije================================================*/
-/*=================================== ostalo================================================*/
+/*===================================================================================================================================*/
 
 var btnDelete = document.getElementById("delete");
 
 btnDelete.onclick = function() {
     writes.value = '';
+
 };
 
 var btnOneDelete = document.getElementById("oneDelete");
 
 btnOneDelete.onclick = function() {
-    writes.value = writes.value.substring(0, writes.value.length - 1);
+    account = account.substring(0, account.length - 1);
+    writes.value = account;
     writes.focus();
 };
+
 
 var btnComa = document.getElementById("coma");
 
 btnComa.onclick = function() {
-    var textLength = writes.value.length;
-    writes.value += '.';
-    for (var i = 0; i < textLength; i++) {
-        if (writes.value[i] === '.') {
-            writes.value = writes.value.substring(0, writes.value.length - 1);
-        }
-        if (writes.value[i] === '-' || writes.value[i] === '+' || writes.value[i] === 'x' || writes.value[i] === '/') {
-            writes.value += '.';
 
-        }
+    var textLength = account.length;
+
+    if (!isNaN(account[textLength - 1])) {
+        account += '.';
     }
+    writes.value = num2;
     writes.focus();
 };
 
@@ -102,18 +174,21 @@ var btnSquareRoot = document.getElementById("Squareroot");
 
 btnSquareRoot.onclick = function() {
 
-    if (!isNaN(writes.value)) {
-        writes.value = Math.sqrt(writes.value);
+    if (!isNaN(account)) {
+        account = Math.sqrt(account);
+        writes.value = account;
     }
     writes.focus();
 };
+
 
 var btnSquare = document.getElementById("Square");
 
 btnSquare.onclick = function() {
 
-    if (!isNaN(writes.value)) {
-        writes.value = Math.pow(writes.value, 2);
+    if (!isNaN(account)) {
+        account = Math.pow(account, 2);
+        writes.value = account;
     }
     writes.focus();
 };
@@ -122,12 +197,11 @@ var btnPercent = document.getElementById("Percent");
 
 btnPercent.onclick = function() {
 
-    var textLength = writes.value.length;
+    var textLength = account.length;
     for (var i = 0; i < textLength; i++) {
-        if (writes.value[i] === '/') {
-            writes.value1 = writes.value.substring(0, writes.value.indexOf('/'));
-            writes.value2 = writes.value.substring(writes.value.indexOf('/') + 1);
-            writes.value = writes.value1 / writes.value2 * 100;
+        if (account[i] === '/') {
+            account = num1 / num2 * 100;
+            writes.value = account;
         }
     }
     writes.focus();
