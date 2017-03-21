1e8bcddb73e26898760f2bc65044529e9e4ad150
nikola Djuric <nikola.8@gmail.com>
2017-03-21 23:59:44 +0100

    classScreen

--- calculator.js
+++ calculator.js
@@ -1,66 +1,88 @@
-var writes = document.getElementById('screen');
-var currNumber = 0;
-var result = 0;
+(function() {
+    "use strict";
+    var result = 0;
 
-var buttons = document.querySelector('.allButtons');
-var numberButtons = buttons.querySelectorAll('.number');
+    var buttons = document.querySelector('.allButtons');
+    var numberButtons = buttons.querySelectorAll('.number');
 
-for (var i = 0; i <= numberButtons.length - 1; i++) {
-
-    numberButtons[i].onclick = function() {
-
-        var buttonNumber = parseInt(this.innerHTML);
-
-        currNumber = currNumber * 10 + buttonNumber
-
-        writes.value = currNumber;
-
+    function Screen() {
+        this._element = document.getElementById('screen');
+        this.reset = false;
     }
-};
 
-var operationButtons = buttons.querySelectorAll('.operation');
+    Screen.prototype.resetOnNextInput = function() {
+        this.reset = true;
+    }
 
-for (var i = 0; i <= operationButtons.length - 1; i++) {
-    var isInitialAction = true;
-    var prevOperation;
-    operationButtons[i].onclick = function() {
+    Screen.prototype.setNumber = function(number) {
+        this._element.value = number;
+    }
 
-        var operation = this.innerHTML;
+    Screen.prototype.getNumber = function() {
+        return this._element.value;
 
- /*===================================================================*/
-     
-     if (operation === this.innerHTML) {
+    };
 
-            if (isInitialAction) {
-                result = currNumber;
-                currNumber = 0;
-                writes.value = result;
-                isInitialAction = false;
-            }
-        else {
+    Screen.prototype.addDigit = function(digit) {
 
-                if (prevOperation === '+') {
-                    result = result + currNumber;
-                    currNumber = 0;
-                    writes.value = result;
-                }
-                if (prevOperation === '-') {
-                    result = result - currNumber;
-                    currNumber = 0;
-                    writes.value = result;
-                }
-                if (prevOperation === 'x') {
-                    result = result * currNumber;
-                    currNumber = 0;
-                    writes.value = result;
-                }
-                if (prevOperation === '/') {
-                    result = result / currNumber;
-                    currNumber = 0;
-                    writes.value = result;
-                }
-            }
-          prevOperation = this.innerHTML;
+        if (this.reset) {
+            this._element.value = digit;
+            this.reset = false;
+        } else {
+            this._element.value += digit;
         }
     }
-}
+
+    var screen = new Screen();
+
+    for (var i = 0; i <= numberButtons.length - 1; i++) {
+
+        numberButtons[i].onclick = function() {
+            var buttonDigit = parseInt(this.innerHTML);
+            screen.addDigit(buttonDigit);
+        }
+    };
+
+    var operationButtons = buttons.querySelectorAll('.operation');
+
+    for (var i = 0; i <= operationButtons.length - 1; i++) {
+        var isInitialAction = true;
+        var prevOperation;
+        var currNumber;
+        operationButtons[i].onclick = function() {
+
+            if (isInitialAction) {
+
+                currNumber = screen.getNumber();
+                result = currNumber;
+                isInitialAction = false;
+
+            } else {
+                currNumber = screen.getNumber();
+
+                switch (prevOperation) {
+
+                    case '+':
+                        result = Number(result) + Number(currNumber);
+                        break;
+
+                    case '-':
+                        result = Number(result) - Number(currNumber);
+                        break;
+
+                    case 'x':
+                        result = Number(result) * Number(currNumber);
+                        break;
+
+                    case '/':
+                        result = Number(result) / Number(currNumber);
+                        break;
+                }
+                screen.setNumber(result);
+            }
+
+            screen.resetOnNextInput();
+            prevOperation = this.innerHTML;
+        }
+    }
+})();
