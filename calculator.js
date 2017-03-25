5c64f5e7b364d1ccdc3c59441e2167c058393d03
nikola Djuric <nikola.8@gmail.com>
2017-03-25 17:07:15 +0100

    addScreenClass

--- calculator.js
+++ calculator.js
@@ -1,39 +1,9 @@
-(function() {
+(function(global) {
     "use strict";
     var result = 0;
 
     var buttons = document.querySelector('.allButtons');
     var numberButtons = buttons.querySelectorAll('.number');
-
-    function Screen() {
-        this._element = document.getElementById('screen');
-        this.reset = false;
-    }
-
-    Screen.prototype.resetOnNextInput = function() {
-        this.reset = true;
-    }
-
-    Screen.prototype.setNumber = function(number) {
-        this._element.value = number;
-    }
-
-    Screen.prototype.getNumber = function() {
-        return this._element.value;
-
-    };
-
-    Screen.prototype.addDigit = function(digit) {
-
-        if (this.reset) {
-            this._element.value = digit;
-            this.reset = false;
-        } else {
-            this._element.value += digit;
-        }
-    }
-
-    var screen = new Screen();
 
     for (var i = 0; i <= numberButtons.length - 1; i++) {
 
@@ -48,17 +18,15 @@
     for (var i = 0; i <= operationButtons.length - 1; i++) {
         var isInitialAction = true;
         var prevOperation;
-        var currNumber;
+
         operationButtons[i].onclick = function() {
+            var currNumber = screen.getNumber();
 
             if (isInitialAction) {
-
-                currNumber = screen.getNumber();
                 result = currNumber;
                 isInitialAction = false;
 
             } else {
-                currNumber = screen.getNumber();
 
                 switch (prevOperation) {
 
@@ -78,11 +46,10 @@
                         result = Number(result) / Number(currNumber);
                         break;
                 }
-                screen.setNumber(result);
             }
-
             screen.resetOnNextInput();
+            screen.setNumber(result);
             prevOperation = this.innerHTML;
         }
     }
-})();
+})(window);
